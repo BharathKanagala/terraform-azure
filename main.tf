c6ce4038bfd7841b3190eb7a6883f54fc1f7916d
bharath kanagala <bharathkanagala19@gmail.com>
2022-09-14 22:43:56 +0530

    configure container resource

--- main.tf
+++ main.tf
@@ -19,5 +19,22 @@ resource "azurerm_resource_group" "spring_tf" {
   location = "westus2"
 }
 
-
+resource "azurerm_container_group" "spring_cg" {
+  location            = azurerm_resource_group.spring_tf.location
+  name                = "api_hello"
+  os_type             = "Linux"
+  resource_group_name = azurerm_resource_group.spring_tf.name
+  ip_address_type     = "Public"
+  dns_name_label      = "hello"
+  container {
+    cpu    = 1
+    image  = "k777/petclinic"
+    memory = 1
+    name   = "springapi"
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
 
